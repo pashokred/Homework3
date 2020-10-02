c83f770896ab1747a2deea99ff06c480426f523b
Pashok <pavlikredko@live.com>
2020-10-02 16:24:02 +0300

    Changed many features, big rebuild

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,63 +4,86 @@ namespace Task3
 {
     class Program
     {
+
         class Converter
         {
-            public Converter(double usd, double eur)
+            public Converter(double currencyCourse)
             {
-                this.usd = usd;
-                this.eur = eur;
+                CurrencyCourse = currencyCourse;
             }
 
-            public double Convert(double uah, string currency)
+            public double ToUah(double currency)
             {
-                if (currency == "usd")
-                {
-                    return uah/usd;
-                }
-                else if (currency == "eur")
-                {
-                    return uah/eur;
-                }
-                else
-                {
-                    return 0;
-                }
+                return currency * CurrencyCourse;
             }
-
-            private double usd;
-            private double eur;
-
+            public double UahTo(double uah)
+            {
+                return uah / CurrencyCourse;
+            }
+            public double CurrencyCourse { protected set; get; }
+        }
+        enum Exchange
+        {
+            USDtoUAH,
+            EURtoUAH,
+            UAHtoEUR,
+            UAHtoUSD,
         }
         
         static void Main(string[] args)
         {
             Console.WriteLine("Please, write double values with dot, like: 28.5  but not: 28,5");
+            Console.WriteLine("Enter number that define operation you want to do:\n" +
+                              "0: USD - UAH\n" +
+                              "1: EUR - UAH\n" +
+                              "2: UAH - EUR\n" +
+                              "3: UAH - USD");
 
-            
-            Console.WriteLine("Enter value of dollar regards to hryvna");
-            double usd = Double.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Enter value of euro regards to hryvnya");
-            double eur = Double.Parse(Console.ReadLine());
-            
-            Converter converter = new Converter(usd, eur);
-            
-            Console.WriteLine("Enter amount of money in hryvna");
-            double uah = Double.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Enter currency in what you want to convert hryvnya (available usd and eur)");
-            string currency = Console.ReadLine();
-
-            double convertedAmount = converter.Convert(uah, currency);
-
-            if (convertedAmount == 0)
+            try
             {
-                Console.WriteLine("Unknown currency {0}", currency);
+
             }
-            else
+            catch (Exception e)
             {
-                Console.WriteLine("It will be {0} {1} for {2} hryvnas", convertedAmount, currency, uah);
+                Console.WriteLine(e);
+                throw;
+            }
+            var operation = (Exchange) int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter value of your currency regards to hryvna");
+            Converter converter = new Converter(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Enter amount of currency you want to convert");
+            double amount = Double.Parse(Console.ReadLine());
+
+
+            try
+            {
+                switch (operation)
+                {
+                    case Exchange.USDtoUAH:
+                        Console.WriteLine("This will be {0} UAH for {1} USD",
+                            Convert.ToDouble($"{converter.ToUah(amount):0.00}"), amount);
+                        break;
+                    case Exchange.EURtoUAH:
+                        Console.WriteLine("This will be {0} UAH for {1} EUR",
+                            Convert.ToDouble($"{converter.ToUah(amount):0.00}"), amount);
+                        break;
+                    case Exchange.UAHtoEUR:
+                        Console.WriteLine("This will be {0} EUR for {1} UAH",
+                            Convert.ToDouble($"{converter.UahTo(amount):0.00}"), amount);
+                        break;
+                    case Exchange.UAHtoUSD:
+                        Console.WriteLine("This will be {0} USD for {1} UAH",
+                            Convert.ToDouble($"{converter.UahTo(amount):0.00}"), amount);
+                        break;
+                    default:
+                        Console.WriteLine("Operation {0} doesn't exist", operation);
+                        break;
+                }
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Division of {0} by zero. Exception said: {1}", amount, e);
+                throw;
             }
         }
     }
